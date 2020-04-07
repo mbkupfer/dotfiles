3527a7179c98575de945800a697f9c2d3ce61962
Maxim Kupfer <mkupfer@staffingindustry.com>
2020-04-07 14:09:29 -0700

    No longer using bash

--- bash/.config/shell/aliases.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-alias ls='ls -lo'
-alias sp='source ~/.bash_profile'
-alias rmraf='rm -rf'
--- bash/.config/shell/environ.sh
+++
@@ -1,8 +0,0 @@
-# ENVIRONMENT_VARIABLES
-
-export FLASK_ENV="development" 
-export CLICOLOR=1
-export LSCOLORS=ExFxBxDxCxegedabagacad
-export TERM="xterm-256color"
-export EDITOR="vim"
-
